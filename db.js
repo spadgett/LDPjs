3d909db297e0de0fdb7a8ca4b25fbe972a41dffc
Samuel Padgett <spadgett@us.ibm.com>
2014-07-25 16:28:07 -0400

    database optimizations

--- db.js
+++ db.js
@@ -1,8 +1,6 @@
 var db;
 var ldp = require('./vocab/ldp.js');			// LDP vocabulary
 
-exports.graphs;
-
 function graphs() {
 	return db.collection('graphs');
 }
@@ -11,6 +9,13 @@ exports.init = function(env, callback) {
 	require('mongodb').connect(env.mongoURL, function(err, conn) {
 		db = conn;
 		exports.graphs = graphs();
+		// index the graph name for fast lookups
+		exports.graphs.ensureIndex( { name: 1 }, { unique: true }, function(err) {
+			if (err) {
+				// not fatal, but log the error
+				console.log(err.stack);
+			}
+		});
 		callback(err);
     });
 }
@@ -36,7 +41,7 @@ exports.put = function(uri, containedBy, interactionModel, triples, callback) {
 
 exports.get = function(uri, callback) {
 	console.log('db.get');
-	graphs().find({ name: uri }, {}).toArray(function(err, docs) {
+	graphs().find({ name: uri }, { limit: 1 }).toArray(function(err, docs) {
 		if (docs && docs[0]) {
 			console.dir(docs[0]);
 			callback(err, docs[0].triples, docs[0].interactionModel);
@@ -51,13 +56,13 @@ exports.remove = function(uri, callback) {
 };
 
 exports.isContainer = function(uri, callback) {
-	graphs().find( { name: uri, interactionModel: ldp.BasicContainer }).count(function(err, count) {
+	graphs().find( { name: uri, interactionModel: ldp.BasicContainer }, { limit: 1 }).count(function(err, count) {
 		callback(err, count);
 	});
 };
 
 exports.getContainment = function(uri, callback) {
-	graphs().find( { containedBy: uri }, { name: 1 }).toArray(function(err, docs) {
+	graphs().find( { containedBy: uri }, { fields: { name: 1 } }).toArray(function(err, docs) {
 		var result = [];
 		if (docs) {
 			docs.forEach(function(doc) {
