b47ecd0849ea5729900ab95b0dd75250c89ff81c
Samuel Padgett <spadgett@us.ibm.com>
2014-08-28 10:23:23 -0400

    normalize URIs

--- turtle.js
+++ turtle.js
@@ -1,7 +1,16 @@
 var N3 = require('n3');
+var url = require('url');
+var path = require('path');
 var media = require('./media.js');				// media types
 var ldp = require('./vocab/ldp.js');			// LDP vocabulary
 var rdf = require('./vocab/rdf.js');			// RDF vocabulary
+
+// normalize paths (remove '.' and '..')
+function normalize(urlStr) {
+	var urlObj = url.parse(urlStr);
+	urlObj.pathname = path.normalize(urlObj.pathname);
+	return urlObj.format();
+}
 
 exports.serialize = function(triples, callback) {
 	var writer = N3.Writer();
@@ -17,6 +26,10 @@ exports.parse = function(req, resourceURI, callback) {
 		if (err) {
 			callback(err);
 		} else if (triple) {
+			triple.subject = normalize(triple.subject);
+			if (N3.Util.isUri(triple.object)) {
+				triple.object = normalize(triple.object);
+			}
 			triples.push(triple);
 		} else {
 			// when last triple is null, we're done parsing
